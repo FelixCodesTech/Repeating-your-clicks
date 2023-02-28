e3481d6d52ec81d19de90cff0c68779e1bd2ec4f
FelixCodesTech <66774630+FelixCodesTech@users.noreply.github.com>
2023-02-28 17:20:22 +0100

    Click positions are saved to file.

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ clicks = []
 
 def esc():
     print('esc was pressed')
+    if len(clicks) > 0:
+        with open("clicks", "w") as file:
+            for click in clicks:
+                file.write(f"{str(click)[1:-1]}\n")
     quit()
+
 key.add_hotkey('esc', esc)
 
 while True:
     key.wait("enter")
     click = pygui.position()
-    clicks.append(click)
+    clicks.append([click.x, click.y])
     print(f"You clicked here: {click}")
-
-print("lol")
